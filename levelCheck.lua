81f95a592df77242de3094241377f3334601f530
Owolskskw <76720185+owlsk@users.noreply.github.com>
2021-02-10 16:25:54 -0800

    Update levelCheck.lua

--- levelCheck.lua
+++ levelCheck.lua
@@ -324,7 +324,7 @@ if levelValue > 89 and levelValue < 95 then
     game:GetService("ReplicatedStorage").remotes.createLobby:InvokeServer(unpack(spInsane))
 end
 
-if tonumber(LevelValue) > 94 then
+if levelValue > 94 and levelValue < 9999 then
     local samuraiPalace = {
         [1] = "Samurai Palace"
     }
